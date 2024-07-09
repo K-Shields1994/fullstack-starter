5aceb075706262ea3344f24794d7204768949044
Kevin Shields <ksjr.22@outlook.com>
2024-07-09 19:14:50 -0400

    Resolved all issues with InventoryController and InventoryControllerTest and changed setting in intelliJ
    Signed-off-by: Kevin Shields <ksjr.22@outlook.com>

--- starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
+++ starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+
 /**
  * Test Inventory DAO.
  */
